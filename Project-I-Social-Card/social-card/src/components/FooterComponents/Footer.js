a7d897a0a582643d3114767034487718541c676e
Aruna Benjamin <arunabenji29@gmail.com>
2019-04-22 18:14:01 -0600

    MVP

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.css';
+const Footer = ()=>{
+    return (
+        <div className='footer'>
+            
+                <i class="far fa-comments"></i>
+            
+            
+                <i class="fas fa-retweet">6</i>
+            
+            
+                <i class="far fa-heart">4</i>
+            
+            
+                <i class="far fa-envelope"></i>
+            
+        </div>
+    );
+}
+export default Footer
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+        <div className = 'header-title'>
+            <div className = 'top'>
+                <h1>Lambda School</h1>
+                <div>@lambdaschool</div>
+                <div>. 26 jan</div>
+            </div>
+            <HeaderContent />
+        </div>
+    );
+}
+export default HeaderTitle
