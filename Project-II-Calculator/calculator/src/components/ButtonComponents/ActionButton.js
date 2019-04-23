b8e28e13406d8ae17c4a45827c3f564ce8920c3b
Aruna Benjamin <arunabenji29@gmail.com>
2019-04-23 14:57:08 -0600

    I did my way of doing MVP

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+import OperatorButton from './OperatorButton';
+
+const operators = ['/','X','-','+','=']
+const ActionButton = () => {
+    return (
+        <div className = 'action-button'>
+            {operators.map(operator => {
+                return <OperatorButton operatorProp = {operator} />;
+            })}
+        </div>
+    );
+}
+export default ActionButton;
